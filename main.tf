bb9761e8327fdc9987c4feb4cdfb1f094de0211f
schmatic <52877366+schmatic@users.noreply.github.com>
2022-10-10 09:32:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,12 @@
-# Create an arbitrary local resource
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
+data "ibm_iam_auth_token" "tokendata" {}
 
-  vars {
-    sample_var = "${var.sample_var}"
-  }
-}
 
-resource "null_resource" "sleep" {
-  triggers {
-    uuid = "${uuid()}"
+resource "null_resource" "print_env_tokens" {
+  triggers = {
+    uuid = uuid()
   }
 
   provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
+    command = "echo IAM: $IC_IAM_TOKEN Refresh: $IC_REFRESH_TOKEN"
   }
- }
+}
